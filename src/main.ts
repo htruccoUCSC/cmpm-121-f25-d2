7a3fd0cfed48557f48ae04afd4e39458dd252307
htruccoUCSC <htrucco@ucsc.edu>
2025-10-16 21:23:56 +0000

    Step 3 complete, added line tracking using an array, and added a drawing changed event.

--- src/main.ts
+++ src/main.ts
@@ -11,29 +11,66 @@ const clear = document.getElementById("clear") as HTMLButtonElement;
 
 const ctx = canvas.getContext("2d")!;
 
+const bus = new EventTarget();
+
+function Notify(eventName: string) {
+  bus.dispatchEvent(new Event(eventName));
+}
+
+bus.addEventListener("drawing-changed", redraw);
+
+interface Point {
+  x: number;
+  y: number;
+}
+const lines: Point[][] = [];
+
+let currentLine: Point[] | null = null;
+
 const cursor = { active: false, x: 0, y: 0 };
 
 canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+
+  currentLine = [];
+  lines.push(currentLine);
+  currentLine.push({ x: cursor.x, y: cursor.y });
+  Notify("drawing-changed");
 });
 
 canvas.addEventListener("mouseup", () => {
   cursor.active = false;
+  currentLine = null;
+  Notify("drawing-changed");
 });
 
 canvas.addEventListener("mousemove", (e) => {
   if (cursor.active) {
-    ctx.beginPath();
-    ctx.moveTo(cursor.x, cursor.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+    currentLine?.push({ x: cursor.x, y: cursor.y });
+    Notify("drawing-changed");
   }
 });
 
 clear.addEventListener("click", () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lines.splice(0, lines.length);
+  Notify("drawing-changed");
 });
+
+function redraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (const line of lines) {
+    if (line.length > 1) {
+      ctx.beginPath();
+      const { x, y } = line[0];
+      ctx.moveTo(x, y);
+      for (const { x, y } of line) {
+        ctx.lineTo(x, y);
+      }
+      ctx.stroke();
+    }
+  }
+}

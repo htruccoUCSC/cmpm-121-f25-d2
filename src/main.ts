2dc61dd8e02b25c4b0873c9ef4ae584915a7fa17
htruccoUCSC <htrucco@ucsc.edu>
2025-10-16 20:48:57 +0000

    Step 2 complete, added the ability for the user to draw using mouse evenets and a clear button clear the screen.

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,38 @@ import "./style.css";
 
 document.body.innerHTML = `
   <h1>Sticker Sheet</h1>
-  <canvas width="256" height="256"></canvas>
+  <canvas id = "canvas" width="256" height="256"></canvas>
+  <button id = "clear">Clear</button>
 `;
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const clear = document.getElementById("clear") as HTMLButtonElement;
+
+const ctx = canvas.getContext("2d")!;
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+clear.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
